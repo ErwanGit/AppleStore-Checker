b21c1476de71f5f813c59200366d2c080d4ab78a
Erwan <erwan977@gmail.com>
2022-10-05 21:54:29 +0200

    feat: ✨ Finish this first version

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,11 @@ import fetch from 'node-fetch';
 import { schedule } from 'node-cron';
 import notifier from 'node-notifier';
 const { notify } = notifier;
+import { iPhone14ProMax } from './types/Products.js';
 
-enum iPhone14ProMax {
-    ClassicForTest = 'MPWP3ZD/A',
-    Purple256 = 'MQ9X3ZD/A',
-    Black1To = 'MQC23ZD/A',
-}
+const findProducts: string[] = [iPhone14ProMax.Silver128, iPhone14ProMax.Silver256];
 
-const findProducts: string[] = [iPhone14ProMax.ClassicForTest];
-
+console.log('Start to check...');
 getAvailableProducts();
 schedule('*/5 * * * *', async () => {
     return getAvailableProducts();
@@ -32,9 +28,9 @@ async function getAvailableProducts() {
             );
 
             notify({
-                title: 'iPhone 14 Pro Max dispo !',
+                title: 'Produit Apple disponible !',
                 message: `${store.partsAvailability[product].messageTypes.regular.storePickupProductTitle} disponible à l'Apple Store ${store.storeName} !`,
-                sound: true,
+                sound: false,
             });
         }
     }
